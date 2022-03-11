de69d43863345c848cab7edbb78de455b8a99b6d
your_github_username <your_github_email>
2022-03-11 14:05:09 +0530

    Welcome_Message

---
+++ src/main/java/com/bridgelabz/Day24/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.Day24;
+
+public class AddressBookMain
+{
+    public static void main(String[] args) {
+        System.out.println( "WELCOME TO ADDRESS BOOK PROGRAM" );
+    }
+}
